6a79b189c77bdd23dc75e3cd798eb799b4109c00
JoseP055 <jospaba12@gmail.com>
2025-07-15 16:32:22 -0600

    Creacion del proyecto v2 - Jose Pablo

---
+++ artesjac-test2/src/App.js
@@ -0,0 +1,14 @@
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Artes JAC</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
