96f20b6b48bc1080f84f2bc032dada90e6ef0cac
adrian <adria@gmail.com>
2025-07-16 19:38:58 -0600

    Agregado Login y Register Page con navegación

--- artesjac-test2/src/routes/AppRouter.js
+++ artesjac-test2/src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink, Link } from 'react-router-dom';
 import { Navbar } from '../components/layout/Navbar';
 import { Footer } from '../components/layout/Footer';
 import { HomePage } from '../pages/HomePage';
@@ -7,7 +7,8 @@ import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
 import { NotFoundPage } from '../pages/NotFoundPage';
 import { BannerFullScreen } from '../components/ui/BannerFullScreen';
-
+import { LoginPage } from '../pages/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage';
 
 export const AppRouter = () => {
     return (
@@ -24,7 +25,10 @@ export const AppRouter = () => {
                         <Route path='/home' element={<HomePage />} />
                         <Route path='/cart' element={<CartPage />} />
                         <Route path='/checkout' element={<CheckoutPage />} />
-                        
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/register' element={<RegisterPage />} />
+                        {/* Aquí puedes agregar más rutas según sea necesario */}
+
                         <Route path='*' element={<NotFoundPage />} />
                     </Routes>
                 </main>
